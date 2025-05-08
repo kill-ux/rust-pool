e410e701876b7257d8d59cc588c59d49aaf679ea
killux <killux>
2025-05-08 10:36:43 +0100

    tem

--- scalar/target/debug/deps/scalar-3933aa93ea10950a.d
+++
@@ -1,5 +0,0 @@
-/home/muboutoub/piscine-rust/scalar/target/debug/deps/libscalar-3933aa93ea10950a.rmeta: src/main.rs
-
-/home/muboutoub/piscine-rust/scalar/target/debug/deps/scalar-3933aa93ea10950a.d: src/main.rs
-
-src/main.rs:
--- scalar/target/debug/deps/scalar-6197ef8f7f9a82a5.d
+++
@@ -1,5 +0,0 @@
-/home/muboutoub/piscine-rust/scalar/target/debug/deps/libscalar-6197ef8f7f9a82a5.rmeta: src/main.rs
-
-/home/muboutoub/piscine-rust/scalar/target/debug/deps/scalar-6197ef8f7f9a82a5.d: src/main.rs
-
-src/main.rs:
