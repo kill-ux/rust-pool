ef444f0f9a70c33bab578071a241aa701a254c42
killux <killux>
2025-05-08 12:23:15 +0100

    divesion

---
+++ division_and_remainder/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn divide(x: i32, y: i32) -> (i32, i32) {
+    (x / y, x % y)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let x = 9;
+        let y = 4;
+        let (division, remainder) = divide(x, y);
+        println!(
+            "{}/{}: division = {}, remainder = {}",
+            x, y, division, remainder
+        );
+    }
+}
