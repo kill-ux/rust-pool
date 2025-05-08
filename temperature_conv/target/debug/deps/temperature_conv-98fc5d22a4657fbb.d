38bc198cec8aa6593bc961ba03f080592f68fa57
killux <killux>
2025-05-08 10:21:22 +0100

    tem

---
+++ temperature_conv/target/debug/deps/temperature_conv-98fc5d22a4657fbb.d
@@ -0,0 +1,5 @@
+/home/muboutoub/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-98fc5d22a4657fbb: src/main.rs
+
+/home/muboutoub/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-98fc5d22a4657fbb.d: src/main.rs
+
+src/main.rs:
---
+++ temperature_conv/target/debug/temperature_conv.d
@@ -0,0 +1,1 @@
+/home/muboutoub/piscine-rust/temperature_conv/target/debug/temperature_conv: /home/muboutoub/piscine-rust/temperature_conv/src/lib.rs /home/muboutoub/piscine-rust/temperature_conv/src/main.rs
