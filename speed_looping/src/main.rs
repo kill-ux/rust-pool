243f792791c83dcdf83a6a7309f6beb09c3b5877
killux <killux>
2025-05-08 11:03:23 +0100

    tem

---
+++ speed_looping/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let mut number = 0;
+    println!("{}", riddle);
+    loop {
+        let mut input = String::new();
+        number += 1;
+        io::stdin().read_line(&mut input).unwrap();
+        if input == "The letter e\n" {
+            println!("{}", number);
+            break;
+        }
+        println!("{}", riddle);
+    }
+}
