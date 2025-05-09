64bb965bb586c81777dea031eb567c4f0cce7ba9
killux <killux>
2025-05-09 10:28:25 +0100

    add ?

---
+++ to_url/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn to_url(s: &str) -> String {
+    s.replace(" ", "%20")
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let s = "Hello, world!";
+        println!("'{}' parsed as an URL becomes '{}'", s, to_url(s));
+    }
+}
