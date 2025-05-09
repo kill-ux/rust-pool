5607ac975e4217015a855a3039e668f8c3421e06
killux <killux>
2025-05-09 11:07:03 +0100

    intial

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut new_vec: Vec<String> = Vec::new();
+    for e in names {
+        let mut vec: Vec<String> = Vec::new();
+        let str = e.split(" ");
+        for e in str {
+            let mut mystr = e.to_string();
+            mystr.replace_range(1..,"");
+            mystr.push('.');
+            vec.push(mystr);
+        }
+        new_vec.push(vec.join(" "));
+    }
+    new_vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
+        println!("{:?}", initials(names));
+    }
+}
