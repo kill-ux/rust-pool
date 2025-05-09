f802ab57d2f23650f365fd506c581bb52ca5005a
killux <killux>
2025-05-09 11:34:42 +0100

    ow

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,16 +1,15 @@
 
 pub fn first_subword(mut s: String) -> String {
     let mut i = false;
-    let mut index = s.len();
     for (j,e) in s.chars().enumerate() {
         if i == false {
             i = true;
         } else {
             if e.is_ascii_uppercase() || e == '_' {
-                index = j;
+                s.replace_range(j.. , "");
+                break;
             }
         }
     }
-    s.replace_range(index.. , "");
     s
 }
