9c593cc292a5d11c24fdec026369abf62bff7c9f
killux <killux>
2025-05-09 11:11:45 +0100

    intial

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,10 +1,11 @@
+
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut new_vec: Vec<String> = Vec::new();
     for e in names {
         let mut vec: Vec<String> = Vec::new();
         let str = e.split_whitespace();
-        for e in str {
-            let mut mystr = e.to_string();
+        for mystr in str {
+            let mut mystr = mystr.to_string();
             mystr.replace_range(1..,".");
             vec.push(mystr);
         }
