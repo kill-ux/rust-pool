3b6a9a7a870532f5ca5cbc6710260ce9a2e2b17a
killux <killux>
2025-05-10 09:45:48 +0100

    tieo

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -7,14 +7,17 @@ pub fn arrange_phrase(phrase: &str) -> String {
     }
 
     for word in phrase.split_whitespace() {
-        // let mut new_str = String::new();
+        let mut new_str = String::new();
         let mut index: usize = 0;
         for c in word.chars() {
             if c.is_numeric() {
                 index = c.to_string().parse().unwrap();
             } 
+            else {
+                new_str.push(c);
+            }
         }
-        new_str1[index-1] = word.replace(&index.to_string(),"");
+        new_str1[index-1] = new_str;
     }
     new_str1.join(" ")
 }
