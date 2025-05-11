0641654a78064be36468ef8bf470cc61ea3cfcad
killux <killux>
2025-05-11 10:35:32 +0100

    edit distance

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -6,7 +6,7 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
         return false;
     }
     let new_s1 = word_frequency_counter(&s1.split("").collect::<Vec<_>>());
-    let new_s2 = word_frequency_counter(&s1.split("").collect::<Vec<_>>());
+    let new_s2 = word_frequency_counter(&s2.split("").collect::<Vec<_>>());
 
     for (ch, num) in new_s1 {
         let count = *new_s2.get(ch).unwrap_or(&0);
