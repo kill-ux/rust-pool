84418711a9f5888b9ce5d5735a7cd6461e24d0af
killux <killux>
2025-05-11 10:10:55 +0100

    hash map

---
+++ bigger/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut count: i32 = 0 ;
+    for (_ , 
+    ) in h.iter() {
+        if num >= count {
+            count = num;
+        }
+    }
+    count
+}
