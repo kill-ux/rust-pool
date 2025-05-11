346deb21f2cb4f9e5cafd69d22d08f6abe973e2c
killux <killux>
2025-05-11 10:31:36 +0100

    edit distance

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -8,7 +8,7 @@ pub fn is_permutation(s1: &str, s2: &str) -> bool {
     let new_s1 = word_frequency_counter(&s1.split("").collect::<Vec<_>>());
     let new_s2 = word_frequency_counter(&s1.split("").collect::<Vec<_>>());
 
-    for (ch, &num) in new_s1.iter() {
+    for (ch, num) in new_s1 {
         let count = *new_s2.get(ch).unwrap_or(&0);
         if count != num {
             return false;
