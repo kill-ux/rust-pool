5c10c9fed9c1d36a674f9c528f91b214d3f4ed6d
killux <killux>
2025-05-11 10:11:06 +0100

    hash map

--- bigger/src/lib.rs
+++ bigger/src/lib.rs
@@ -2,8 +2,7 @@ use std::collections::HashMap;
 
 pub fn bigger(h: HashMap<&str, i32>) -> i32 {
     let mut count: i32 = 0 ;
-    for (_ , 
-    ) in h.iter() {
+    for (_ , &num) in h.iter() {
         if num >= count {
             count = num;
         }
