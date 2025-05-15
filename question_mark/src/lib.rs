41c24332ba08970b8aefbd31d19d44a2174829d9
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-15 13:38:05 +0100

    question_mark

---
+++ question_mark/src/lib.rs
@@ -0,0 +1,21 @@
+pub struct One {
+    pub first_layer: Option<Two>
+}
+pub struct Two {
+    pub second_layer: Option<Three>
+}
+pub struct Three {
+    pub third_layer: Option<Four>
+}
+pub struct Four {
+    pub fourth_layer: Option<u16>
+}
+
+impl One {
+    pub fn get_fourth_layer(self) -> Option<u16> {
+        self.first_layer.unwrap()
+        .second_layer.unwrap()
+        .third_layer.unwrap()
+        .fourth_layer
+    }
+}
