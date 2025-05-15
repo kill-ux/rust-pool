a789a8df21f1ae990b552bf9174de3523fa03606
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-15 18:51:25 +0100

    cipher

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -4,6 +4,9 @@ pub struct CipherError {
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    if ciphered.is_empty() {
+        return Err(CipherError { expected: String::from_utf8("") });
+    }
     let mut new_vec = vec![];
     for (i, &b) in original.as_bytes().iter().enumerate() {
         let mut num = b;
@@ -14,7 +17,7 @@ pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
         }
         new_vec.push(num);
     }
-    
+
     for (i, &a) in ciphered.as_bytes().iter().enumerate() {
         if a != new_vec[i] {
             return Err(CipherError { expected: String::from_utf8(new_vec).unwrap() });
