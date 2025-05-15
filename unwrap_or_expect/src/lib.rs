d7722bba04011273d392089f7d8241f42ae9d70a
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-15 11:18:43 +0100

    unwrap _ expect

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,22 @@
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    let s = match security_level {
+        Security::Unknown => server.unwrap(),
+        Security::Message => server.expect("ERROR: program stops"),
+        Security::Warning => server.unwrap_or("WARNING: check the server"),
+        Security::NotFound => match server {
+            Ok(a) => a,
+            Err(msg_err) => &format!("Not found: {msg_err}")
+         },
+        Security::UnexpectedUrl => server.unwrap(),
+    } ;
+
+    s.to_string()
+}
