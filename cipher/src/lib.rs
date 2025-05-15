fea2fba4933c8dcf4b343071b5df89d769e8a424
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-15 18:48:34 +0100

    cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, PartialEq)]
+pub struct CipherError {
+    pub expected: String,
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
+    let mut new_vec = vec![];
+    for (i, &b) in original.as_bytes().iter().enumerate() {
+        let mut num = b;
+        if b >= 65 && b <= 90 {
+            num = 90 - (b - 65);
+        } else if b >= 97 && b <= 122 {
+            num = 122 - (b - 97);
+        }
+        new_vec.push(num);
+    }
+    
+    for (i, &a) in ciphered.as_bytes().iter().enumerate() {
+        if a != new_vec[i] {
+            return Err(CipherError { expected: String::from_utf8(new_vec).unwrap() });
+        }
+    }
+    Ok(())
+}
