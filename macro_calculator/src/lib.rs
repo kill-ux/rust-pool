efb4bd9ec9be035991b7a4cafae278ef5a124e9e
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-16 16:23:45 +0100

    i think its fail

---
+++ macro_calculator/src/lib.rs
@@ -0,0 +1,32 @@
+pub use json::*;
+
+pub struct Food {
+    pub name: String,
+    pub calories: (String, String),
+    pub fats: f64,
+    pub carbs: f64,
+    pub proteins: f64,
+    pub nbr_of_portions: f64,
+}
+
+pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
+    let mut cals: f64 = 0.0;
+    let mut carbs: f64 = 0.0;
+    let mut proteins: f64 = 0.0;
+    let mut fats: f64 = 0.0;
+    for food in foods {
+        cals += food.calories.1[..food.calories.1.len() - 4]
+            .parse::<f64>()
+            .unwrap()
+            * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions;
+        proteins += food.proteins * food.nbr_of_portions;
+        fats += food.fats * food.nbr_of_portions;
+    }
+    object! {
+        cals : format!("{:.2}",cals),
+        carbs :  format!("{:.2}",carbs),
+        proteins :  format!("{:.2}",proteins),
+        fats :  format!("{:.2}",fats),
+    }
+}
