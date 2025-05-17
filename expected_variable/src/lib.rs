48ca182c6268942b47b85bdda4fdf3dcbf2c0244
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-17 12:17:05 +0100

    float

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -2,13 +2,13 @@ use convert_case::{Case, Casing};
 use edit_distance as Ed;
 
 pub fn expected_variable(expected: &str, s: &str) -> Option<String> {
-    if !s.is_case(Case::Camel) && !s.is_case(Case::Snake) && !s.is_case(Case::UpperCamel) {
+    let s = &s.to_lowercase();
+    let expected = &expected.to_lowercase();
+    if !s.is_case(Case::Camel) && !s.is_case(Case::Snake) {
         None
     } else {
-        let s = &s.to_lowercase();
-        let expected = &expected.to_lowercase();
         let edit = Ed::edit_distance(s, expected);
-        let res = 100 - ((edit * 100) / s.len());
+        let res: i64 = 100 - ((edit as i64 * 100) / s.len() as i64);
         if res <= 50 {
             None
         } else {
