6bc363fa7efff0e9909e49580381ec48471e5bac
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-17 16:08:11 +0100

    stars

---
+++ scores/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn score(s: &str) -> u64 {
+    let mut num = 0;
+    for ch in s.chars() {
+        num += match ch {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+        }
+    }
+}
