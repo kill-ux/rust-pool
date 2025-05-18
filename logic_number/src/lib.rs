fd736a5c9db1ed0e33e18477df79bf750551eac7
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-18 09:44:00 +0100

    number logic

---
+++ logic_number/src/lib.rs
@@ -0,0 +1,10 @@
+fn number_logic(num: u32) -> bool {
+    let n = num.to_string();
+    let le = n.len();
+    let mut sum = 0;
+    for nb in n.chars() {
+       sum += nb.to_string().parse::<u32>().unwrap().pow(le as u32);
+    }
+    println!("{sum}");
+    sum == num 
+}
