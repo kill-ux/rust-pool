f758684e922581a21f18ded299993b96479d5f06
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-12 10:05:27 +0100

    find_factorial

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -1,25 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn factorial(num: u64) -> u64 {
-    if num == 1 || num == 0 {
-        1
-    } else {
-        num * (factorial(num - 1))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        println!("The factorial of 0 = {}", factorial(0));
-        println!("The factorial of 1 = {}", factorial(1));
-        println!("The factorial of 5 = {}", factorial(5));
-        println!("The factorial of 10 = {}", factorial(10));
-        println!("The factorial of 19 = {}", factorial(19));
-    }
+    if num == 1 || num == 0 { return 1 }
+    num * (factorial(num - 1))
 }
--- reverse_string/src/lib.rs
+++ reverse_string/src/lib.rs
@@ -1,7 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn rev_str(input: &str) -> String {
     input.chars().rev().collect()
 }
