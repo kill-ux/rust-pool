a35e8a000c4d6ae37ef79387ba9c2d6ee030679d
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-12 10:00:02 +0100

    groceries

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -5,21 +5,3 @@ pub fn insert(vec: &mut Vec<String>, val: String) {
 pub fn at_index(slice: &[String], index: usize) -> &str {
     &slice[index]
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let mut groceries = vec![
-            "yogurt".to_string(),
-            "panettone".to_string(),
-            "bread".to_string(),
-            "cheese".to_string(),
-        ];
-        insert(&mut groceries, String::from("nuts"));
-        println!("groceries = {:?}", &groceries);
-        println!("groceries[1] = {:?}", at_index(&groceries, 1));
-    }
-}
