42fc0555c4b251d6923a32fa3de887bb4c6df216
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-12 09:55:54 +0100

    speed_transformation

--- speed_transformation/src/lib.rs
+++ speed_transformation/src/lib.rs
@@ -1,15 +1,3 @@
 pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
     km_h * 1000.0 / 3600.0
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let km_h = 100.0;
-        let m_s = km_per_hour_to_meters_per_second(km_h);
-        println!("{} km/h is equivalent to {} m/s", km_h, m_s);
-    }
-}
