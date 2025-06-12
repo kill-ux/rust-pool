43a62dce619667279d4e4918ed97f1e5b740ebe5
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-12 09:57:58 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -2,9 +2,9 @@ use std::io;
 
 fn main() {
     let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
-    let mut number = 0;
-    println!("{}", riddle);
+    let mut number: u32 = 0;
     loop {
+        println!("{}", riddle);
         let mut input = String::new();
         number += 1;
         io::stdin().read_line(&mut input).unwrap();
@@ -12,6 +12,5 @@ fn main() {
             println!("Number of trials: {}", number);
             break;
         }
-        println!("{}", riddle);
     }
 }
