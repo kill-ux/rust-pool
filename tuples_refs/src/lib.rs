cba9a0b506d491863333f38dec8f23bada3a5123
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-12 10:16:04 +0100

    tuples_refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,5 +1,3 @@
-
-
 pub struct Student(pub u32, pub String, pub String);
 
 pub fn id(student: &Student) -> u32 {
@@ -12,17 +10,4 @@ pub fn first_name(student: &Student) -> &str {
 
 pub fn last_name(student: &Student) -> &str {
     &student.2
-}   
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-        println!("Student's first name: {}", first_name(&student));
-        println!("Student's last name: {}", last_name(&student));
-        println!("Student's id: {}", id(&student));
-    }
 }
