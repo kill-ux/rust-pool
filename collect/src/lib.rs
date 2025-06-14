49ef89b8ca97adc77e1a1d253e724037de41c56c
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 12:40:47 +0100

    collect

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -2,7 +2,7 @@ pub fn bubble_sort(arr: &mut [i32]) {
     for _ in 0..arr.len() {
         for j in 0..arr.len() - 1 {
             if arr[j + 1] < arr[j] {
-                let temp = arr[j+1];
+                let temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
