51fe79b17cb757058673000936ff2f4df9042e24
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 12:02:10 +0100

    edit

--- edit_distance/src/main.rs
+++ edit_distance/src/main.rs
@@ -3,10 +3,10 @@ use edit_distance::*;
 fn main() {
     let source = "alignment";
     let target = "assignment";
-
-    // assert_eq!(edit_distance("gumbo", "gambol"), 2);
+    edit_distance("abcdef", "azced");
+    assert_eq!(edit_distance("gumbo", "gambol"), 2);
     assert_eq!(edit_distance("kitten", "sitting"), 3);
-    // assert_eq!(edit_distance("rosettacode", "raisethysword"), 8);
+    assert_eq!(edit_distance("rosettacode", "raisethysword"), 8);
 
     println!(
         "It's necessary to make {} change(s) to {:?} to get {:?}",
