873adc82f2666d548ef57c4c5224ee1b7e03fe3e
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 09:20:33 +0100

    ownership

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,14 +1,8 @@
-
 pub fn first_subword(mut s: String) -> String {
-    let mut i = false;
-    for (j,e) in s.chars().enumerate() {
-        if i == false {
-            i = true;
-        } else {
-            if e.is_ascii_uppercase() || e == '_' {
-                s.replace_range(j.. , "");
-                break;
-            }
+    for (j, e) in s[1..].chars().enumerate() {
+        if e.is_ascii_uppercase() || e == '_' {
+            s.replace_range(j.., "");
+            break;
         }
     }
     s
