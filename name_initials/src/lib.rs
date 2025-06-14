f4ca6ff5d5ed2269f6e3c0be00bbe6452f9aac5e
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 09:11:15 +0100

    name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -2,24 +2,10 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut new_vec: Vec<String> = Vec::new();
     for e in names {
         let mut vec: Vec<String> = Vec::new();
-        let str = e.split(" ");
-        for e in str {
-            let mut mystr = e.to_string();
-            mystr.replace_range(1..,".");
-            vec.push(mystr);
+        for e in e.split(" ") {
+            vec.push(e[0..1].to_string() + ".");
         }
         new_vec.push(vec.join(" "));
     }
     new_vec
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
-        println!("{:?}", initials(names));
-    }
 }
