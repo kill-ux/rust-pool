93daa6a60f08c80d2422768469eca38822fcff45
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 12:39:20 +0100

    string_permutation

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -1,18 +1,14 @@
 use std::collections::HashMap;
 
 pub fn mean(list: &[i32]) -> f64 {
-    let mut count = 0;
-    for num in list {
-        count += num;
-    }
-    (count as f64) / (list.len() as f64)
+    (list.iter().sum::<i32>() as f64) / (list.len() as f64)
 }
 
 pub fn median(arr: &[i32]) -> i32 {
     let mut list = arr.to_vec();
     list.sort();
     let le = list.len();
-    if le % 2 == 0 {
+    if list.len() % 2 == 0 {
         return (list[le / 2] + list[le / 2 - 1]) / 2;
     }
     list[le / 2]
@@ -26,7 +22,7 @@ pub fn mode(list: &[i32]) -> i32 {
             None => new_hash.insert(*num, 1),
         };
     }
-    let mut max = *new_hash.get(&list[0]).unwrap_or(&0);
+    let mut max = usize::MIN;
     let mut last = list[0];
     for (num, le) in new_hash {
         if le > max {
