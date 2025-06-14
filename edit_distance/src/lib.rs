8b2d3729aa71d500700d92f95bea45bc03ee87a4
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 12:09:26 +0100

    edit

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -3,35 +3,44 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     let l2 = target.len();
     let mut matrix: Vec<Vec<usize>> = Vec::with_capacity(l1);
     for i in 0..=l1 {
-        let mut vec2: Vec<usize> = vec![];
+        // let mut vec2: Vec<usize> = vec![];
+
+        matrix.push( vec![]);
         if i == 0 {
             for j in 0..=l2 {
-                vec2.push(j);
+                matrix[i].push(j);
             }
         } else {
-            for k in 0..=l2 {
-                if k == 0 {
-                    vec2.push(i);
+            for j in 0..=l2 {
+                if j == 0 {
+                    matrix[i].push(i);
+                } else if j == 0 && i == 0 {
+                    matrix[i].push(0);
                 } else {
-                    vec2.push(0);
+                    if source[i - 1..i] == target[j - 1..j] {
+                        let a = matrix[i - 1][j - 1];
+                        matrix[i].push(a);
+                    } else {
+                        let min = matrix[i - 1][j - 1].min(matrix[i][j - 1].min(matrix[i - 1][j]));
+                        matrix[i].push(min + 1);
+                    }
                 }
             }
         }
-        matrix.push(vec2);
     }
 
-    for i in 1..=l1 {
-        for j in 1..=l2 {
-            if i != 0 && j != 0 {
-                if source[i-1..i] == target[j-1..j] {
-                    matrix[i][j] = matrix[i - 1][j - 1]
-                } else {
-                    let min = matrix[i - 1][j - 1].min(matrix[i][j - 1].min(matrix[i - 1][j]));
-                    matrix[i][j] = min + 1;
-                }
-            }
-        }
-    }
+    // for i in 1..=l1 {
+    //     for j in 1..=l2 {
+    //         if i != 0 && j != 0 {
+    //             if source[i - 1..i] == target[j - 1..j] {
+    //                 matrix[i][j] = matrix[i - 1][j - 1]
+    //             } else {
+    //                 let min = matrix[i - 1][j - 1].min(matrix[i][j - 1].min(matrix[i - 1][j]));
+    //                 matrix[i][j] = min + 1;
+    //             }
+    //         }
+    //     }
+    // }
 
     matrix[l1][l2]
 }
