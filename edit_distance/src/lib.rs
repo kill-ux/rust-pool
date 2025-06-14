5a9441e6087a142a0e89e94c28ba9fa005da8b38
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-14 12:18:54 +0100

    hash

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -3,8 +3,6 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     let l2 = target.len();
     let mut matrix: Vec<Vec<usize>> = Vec::with_capacity(l1);
     for i in 0..=l1 {
-        // let mut vec2: Vec<usize> = vec![];
-
         matrix.push( vec![]);
         if i == 0 {
             for j in 0..=l2 {
@@ -28,19 +26,5 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
             }
         }
     }
-
-    // for i in 1..=l1 {
-    //     for j in 1..=l2 {
-    //         if i != 0 && j != 0 {
-    //             if source[i - 1..i] == target[j - 1..j] {
-    //                 matrix[i][j] = matrix[i - 1][j - 1]
-    //             } else {
-    //                 let min = matrix[i - 1][j - 1].min(matrix[i][j - 1].min(matrix[i - 1][j]));
-    //                 matrix[i][j] = min + 1;
-    //             }
-    //         }
-    //     }
-    // }
-
     matrix[l1][l2]
 }
--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -3,10 +3,7 @@ use std::collections::HashMap;
 pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
     let mut new_hash: HashMap<&'a str, usize> = HashMap::new();
     for word in words {
-        match new_hash.get(word) {
-            Some(num) => new_hash.insert(word, num + 1),
-            None => new_hash.insert(word, 1),
-        };
+        new_hash.entry(*word).and_modify(|c| *c += 1 ).or_insert(1);
     }
     new_hash
 }
