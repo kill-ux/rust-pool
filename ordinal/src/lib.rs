7ec769ac6680b5273d0f601d773e2f52fa6cb69b
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 15:08:48 +0100

    ordenaly

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,15 +1,11 @@
 pub fn num_to_ordinal(x: u32) -> String {
     match x {
         11 | 12 | 13 => x.to_string() + "th",
-        _ => {
-            let a = x.to_string();
-            let l = a.len();
-            match &a[l - 1..] {
-                "1" => a + "st",
-                "2" => a + "nd",
-                "3" => a + "rd",
-                _ => a + "th",
-            }
-        }
+        _ => match x % 10 {
+            1 => x.to_string() + "st",
+            2 => x.to_string() + "nd",
+            3 => x.to_string() + "rd",
+            _ => x.to_string() + "th",
+        },
     }
 }
