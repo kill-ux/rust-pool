5ebff15463fb4dd5d566327c0758c43f8e1b8a00
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 15:14:13 +0100

    is pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,7 +3,9 @@ use std::collections::HashMap;
 pub fn is_pangram(s: &str) -> bool {
     let mut map: HashMap<char, bool> = HashMap::new();
     for ele in s.chars() {
-        map.insert(ele, true);
+        if ele.is_alphabetic() {
+            map.insert(ele, true);
+        }
     }
     map.len() == 26
 }
