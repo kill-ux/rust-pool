28f0ae19610c863d0dc167a61ff1b6e0751ff384
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 09:53:56 +0100

    middle day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,10 +1,10 @@
-use chrono::prelude::*;
+use chrono::*;
 
 pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
-    let y = NaiveDate::from_ymd_opt(year as i32, 7, 2).unwrap();
-    if y.leap_year() {
+    let date = chrono::NaiveDate::from_ymd_opt(year as i32, 7, 2).unwrap();
+    if date.leap_year() {
         None
     } else {
-        Some(y.weekday())
+        Some(date.weekday())
     }
 }
