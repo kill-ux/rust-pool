6c3ee381066dfbac9e50997d5c81c0ca939c30c2
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 13:11:51 +0100

    scores

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn score(s: &str) -> u64 {
     let mut num = 0;
-    for ch in s.chars() {
-        num += match ch.to_uppercase().next().unwrap() {
+    for c in s.chars() {
+        num += match c.to_uppercase().nth(0).unwrap() {
             'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
             'D' | 'G' => 2,
             'B' | 'C' | 'M' | 'P' => 3,
@@ -9,7 +9,7 @@ pub fn score(s: &str) -> u64 {
             'K' => 5,
             'J' | 'X' => 8,
             'Q' | 'Z' => 10,
-            _ => 0
+            _ => 0,
         };
     }
     num
