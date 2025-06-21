7f1813d6391433742ed290fb3c0cfb883d3db439
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 12:30:35 +0100

    mall

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -1,81 +1,68 @@
-pub mod mall;
+mod mall;
+use std::iter::Map;
+
 pub use mall::*;
-use std::collections::HashMap;
 
-pub fn biggest_store(m: &Mall) -> (String, Store) {
-    let mut store = Store {
-        employees: HashMap::new(),
-        square_meters: 0,
-    };
-    let mut k: String = String::new();
-    for (_, f) in &m.floors {
-        for (key, s) in &f.stores {
-            if s.square_meters > store.square_meters {
-                store = s.clone();
-                k = key.clone();
+pub fn biggest_store(mall: &Mall) -> (String, Store) {
+    mall.floors
+        .values()
+        .flat_map(|a| a.stores.clone())
+        .max_by(|a, b| a.1.square_meters.cmp(&b.1.square_meters))
+        .unwrap()
+}
+
+pub fn highest_paid_employee(mall: &Mall) -> Vec<(&str, Employee)> {
+    let mut res: Vec<(&str, Employee)> = Vec::new();
+    let mut last_salary = 0.;
+    mall.floors
+        .values()
+        .flat_map(|a| a.stores.iter())
+        .flat_map(|a| a.1.employees.iter())
+        .for_each(|a| {
+            if a.1.salary == last_salary {
+                res.push((a.0, a.1.clone()));
+            } else if a.1.salary > last_salary {
+                res.clear();
+                res.push((a.0, a.1.clone()));
+                last_salary = a.1.salary;
             }
-        }
-    }
-
-    (k, store)
-}
-
-pub fn highest_paid_employee(m: &Mall) -> Vec<(&str, Employee)> {
-    let mut vec: Vec<(&str, Employee)> = Vec::new();
-    let mut salary = 0.0;
-
-    for (_, f) in &m.floors {
-        for (_, s) in &f.stores {
-            for (key, e) in &s.employees {
-                if e.salary == salary {
-                    vec.push((key, e.clone()));
-                } else if e.salary > salary {
-                    salary = e.salary;
-                    vec = vec![(key, e.clone())]
-                }
-            }
-        }
-    }
-
-    vec
-}
-
-pub fn nbr_of_employees(m: &Mall) -> usize {
-    let mut num: usize = m.guards.len();
-    for (_, f) in &m.floors {
-        for (_, s) in &f.stores {
-            num += s.employees.len();
-        }
-    }
-    num
-}
-
-pub fn check_for_securities(m: &mut Mall, mut map: Vec<(String, Guard)>) {
-    let mut number = 0;
-    m.floors.values().for_each(|flr| {
-        number += flr.size_limit;
-    });
-
-    let num = number / 200;
-    let l = m.guards.len() as u64;
-    if l < num {
-        for _ in 0..num - l {
-            let (key, value) = map.pop().unwrap();
-            m.guards.insert(key, value);
-        }
-    }
-}
-
-pub fn cut_or_raise(m: &mut Mall) {
-    m.floors.values_mut().for_each(|flr| {
-        flr.stores.values_mut().for_each(|st| {
-            st.employees.values_mut().for_each(|emp| {
-                if emp.working_hours.1 - emp.working_hours.0 >= 10 {
-                    emp.raise(emp.salary * 0.1);
-                } else {
-                    emp.cut(emp.salary * 0.1);
-                }
-            });
         });
-    });
+    res
+}
+
+pub fn nbr_of_employees(mall: &Mall) -> usize {
+    mall.guards.len()
+        + mall
+            .floors
+            .values()
+            .flat_map(|a| a.stores.clone())
+            .flat_map(|a| a.1.employees.clone())
+            .collect::<Vec<_>>()
+            .len()
+}
+
+pub fn check_for_securities(mall: &mut Mall, mut map: Vec<(String, Guard)>) {
+    let mut size = 0;
+    mall.floors.values().for_each(|f| size += f.size_limit);
+    let times = size / 200;
+    let g = mall.guards.len() as u64;
+    for _ in g..times {
+        let a = map.pop().unwrap();
+        mall.hire_guard(a.0, a.1);
+    }
+}
+
+pub fn cut_or_raise(mall: &mut Mall) {
+    mall.floors
+        .values_mut()
+        .flat_map(|a| a.stores.values_mut())
+        .flat_map(|a| a.employees.values_mut())
+        .for_each(|f| {
+            let r = f.working_hours.1 - f.working_hours.0;
+            if r > 10 {
+                f.raise(0.1 * f.salary);
+            } else {
+                f.cut(0.1 * f.salary);
+            }
+        });
 }
