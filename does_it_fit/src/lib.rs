13055140774b8bd8459131630a6166cc8f6ab486
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 10:15:24 +0100

    area

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,4 +1,4 @@
-mod areas_volumes ;
+mod areas_volumes;
 use areas_volumes::*;
 
 pub fn area_fit(
@@ -8,12 +8,13 @@ pub fn area_fit(
     (a, b): (usize, usize),
 ) -> bool {
     let rec = rectangle_area(x, y) as f64;
-    let area_times = times as f64 *  match kind {
-        GeometricalShapes::Square => square_area(a) as f64,
-        GeometricalShapes::Circle => circle_area(a),
-        GeometricalShapes::Rectangle => rectangle_area(a,b)  as f64,
-        GeometricalShapes::Triangle => triangle_area(a,b)  as f64,
-    };
+    let area_times = times as f64
+        * match kind {
+            GeometricalShapes::Square => square_area(a) as f64,
+            GeometricalShapes::Circle => circle_area(a),
+            GeometricalShapes::Rectangle => rectangle_area(a, b) as f64,
+            GeometricalShapes::Triangle => triangle_area(a, b) as f64,
+        };
     rec >= area_times
 }
 
@@ -23,13 +24,14 @@ pub fn volume_fit(
     times: usize,
     (a, b, c): (usize, usize, usize),
 ) -> bool {
-    let rec = cube_volume(x) as f64;
-    let area_times = times as f64 *  match kind {
-        GeometricalVolumes::Cone => cone_volume(a,b),
-        GeometricalVolumes::Cube => cube_volume(a) as f64,
-        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a,b,c)  as f64,
-        GeometricalVolumes::Sphere => sphere_volume(a),
-        GeometricalVolumes::TriangularPyramid => triangular_pyramid_volume(a as f64,b)  as f64,
-    };
+    let rec = parallelepiped_volume(x, y, z) as f64;
+    let area_times = times as f64
+        * match kind {
+            GeometricalVolumes::Cone => cone_volume(a, b),
+            GeometricalVolumes::Cube => cube_volume(a) as f64,
+            GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
+            GeometricalVolumes::Sphere => sphere_volume(a),
+            GeometricalVolumes::TriangularPyramid => triangular_pyramid_volume(a as f64, b) as f64,
+        };
     rec >= area_times
 }
