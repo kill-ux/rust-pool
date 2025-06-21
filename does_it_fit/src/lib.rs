a81d5e0536e7e2145884d954e5e18ea470244408
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 10:13:13 +0100

    area

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,5 +1,5 @@
-mod areas_volumes;
-pub use areas_volumes::*;
+mod areas_volumes ;
+use areas_volumes::*;
 
 pub fn area_fit(
     (x, y): (usize, usize),
@@ -7,14 +7,14 @@ pub fn area_fit(
     times: usize,
     (a, b): (usize, usize),
 ) -> bool {
-    let area_rec = rectangle_area(x, y);
-    let area_kind = match kind {
+    let rec = rectangle_area(x, y) as f64;
+    let area_times = times as f64 *  match kind {
         GeometricalShapes::Square => square_area(a) as f64,
         GeometricalShapes::Circle => circle_area(a),
-        GeometricalShapes::Rectangle => rectangle_area(a, b) as f64,
-        GeometricalShapes::Triangle => triangle_area(a, b) as f64,
+        GeometricalShapes::Rectangle => rectangle_area(a,b)  as f64,
+        GeometricalShapes::Triangle => triangle_area(a,b)  as f64,
     };
-    (area_kind * times as f64) <= area_rec as f64
+    rec >= area_times
 }
 
 pub fn volume_fit(
@@ -23,13 +23,13 @@ pub fn volume_fit(
     times: usize,
     (a, b, c): (usize, usize, usize),
 ) -> bool {
-    let volume = parallelepiped_volume(x, y, z);
-    let area_kind = match kind {
+    let rec = cube_volume(x) as f64;
+    let area_times = times as f64 *  match kind {
+        GeometricalVolumes::Cone => cone_volume(a,b),
         GeometricalVolumes::Cube => cube_volume(a) as f64,
-        GeometricalVolumes::Cone => cone_volume(a, b) as f64,
-        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
-        GeometricalVolumes::Sphere => sphere_volume(a) as f64,
-        GeometricalVolumes::TriangularPyramid => triangular_pyramid_volume(a as f64, b) as f64,
+        GeometricalVolumes::Parallelepiped => parallelepiped_volume(a,b,c)  as f64,
+        GeometricalVolumes::Sphere => sphere_volume(a),
+        GeometricalVolumes::TriangularPyramid => triangular_pyramid_volume(a as f64,b)  as f64,
     };
-    (area_kind * times as f64) <= volume as f64
+    rec >= area_times
 }
