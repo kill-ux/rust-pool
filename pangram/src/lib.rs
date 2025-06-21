f050f2535008b55e772ee0cabf241305dd4ef657
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 15:24:08 +0100

    is pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 pub fn is_pangram(s: &str) -> bool {
     let mut map: HashMap<char, bool> = HashMap::new();
     for ele in s.chars() {
-        if ele.is_alphabetic() {
-            map.insert(ele, true);
+        if ele.is_ascii_alphabetic() {
+            map.insert(ele.to_lowercase().nth(0).unwrap(), true);
         }
     }
     map.len() == 26
--- pangram/src/main.rs
+++ pangram/src/main.rs
@@ -1,9 +1,14 @@
 use pangram::*;
 
-fn main() {
-    println!(
-        "{}",
-        is_pangram("the quick brown fox jumps over the lazy dog!")
-    );
-    println!("{}", is_pangram("this is not a pangram!"));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    
+    #[test]
+    fn test_not_pangram() {
+        assert!(is_pangram(
+            "Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich."
+        ));
+    }
 }
