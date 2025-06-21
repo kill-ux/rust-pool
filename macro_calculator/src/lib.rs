b067141465498c452220d0972224645ae736a727
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-21 10:40:41 +0100

    food

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -15,16 +15,16 @@ pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
     let mut proteins = 0.;
     let mut fats = 0.;
     foods.iter().for_each(|food| {
-        cals = food.calories.1.parse::<f64>().unwrap() * food.nbr_of_portions;
-        carbs = food.carbs * food.nbr_of_portions;
-        proteins = food.proteins * food.nbr_of_portions;
-        fats = food.fats * food.nbr_of_portions;
+        cals += food.calories.1[..food.calories.1.len() - 4].parse::<f64>().unwrap() * food.nbr_of_portions;
+        carbs += food.carbs * food.nbr_of_portions;
+        proteins += food.proteins * food.nbr_of_portions;
+        fats += food.fats * food.nbr_of_portions;
     });
 
     object! {
-        cals : format!("{cals}").parse::<f64>().unwrap() ,
-        carbs: format!("{carbs}").parse::<f64>().unwrap(),
-        proteins: format!("{proteins}").parse::<f64>().unwrap() ,
-        fats: format!("{fats}").parse::<f64>().unwrap()
+        cals : format!("{cals:.2}").parse::<f64>().unwrap() ,
+        carbs: format!("{carbs:.2}").parse::<f64>().unwrap(),
+        proteins: format!("{proteins:.2}").parse::<f64>().unwrap() ,
+        fats: format!("{fats:.2}").parse::<f64>().unwrap()
     }
 }
