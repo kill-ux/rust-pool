dcaf85ebf1599af74b2d516c763966a4dbe7b558
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-22 12:56:10 +0100

    diamoond

--- diamond_creation/src/lib.rs
+++ diamond_creation/src/lib.rs
@@ -3,12 +3,19 @@ pub fn get_diamond(c: char) -> Vec<String> {
     let mut spaces = (c as u8 - b'A') as usize;
     let mut index: usize = 0;
     for ch in 'A'..=c {
+        // spaces
         let mut str = " ".repeat(spaces);
+        // charachter
         str.push(ch);
+
         if index != 0 {
+            // spaces inside
             str.push_str(&" ".repeat(index * 2 - 1));
+            // ch
             str.push(ch);
         }
+
+        // spaces
         str.push_str(&" ".repeat(spaces));
 
         vec.push(str);
