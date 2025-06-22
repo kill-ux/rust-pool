024f71806de2f2ff554c8e77a3d5d2d1478f6c0e
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-22 09:53:46 +0100

    vowels

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,16 +1,13 @@
-
-
 pub fn pig_latin(text: &str) -> String {
-    let vowels = "aeiou";
-    if !vowels.contains(text.chars().next().unwrap()) && &text[1..3] == "qu" {
-        return  text[3..].to_string() + &text[..3] + "ay" ;
+    if !"aeiou".contains(text.chars().next().unwrap()) && &text[1..3] == "qu" {
+        return text[3..].to_string() + &text[..3] + "ay";
     }
 
-    for (i,ch) in text.char_indices() {
-        if vowels.contains(ch) {
-            return  text[i..].to_string() + &text[..i] + "ay" ;
+    for (i, ch) in text.char_indices() {
+        if "aeiou".contains(ch) {
+            return text[i..].to_string() + &text[..i] + "ay";
         }
     }
-
+    
     text.to_string()
 }
