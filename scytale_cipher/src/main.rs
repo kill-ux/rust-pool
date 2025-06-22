4c58955a1f3561fcf1c2c6dedc73b889933910c9
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-06-22 12:52:56 +0100

    diamoond

--- scytale_cipher/src/main.rs
+++ scytale_cipher/src/main.rs
@@ -3,5 +3,5 @@ use scytale_cipher::scytale_cipher;
 fn main() {
     println!("\"scytale Code\" size=6 -> {:?}", scytale_cipher(String::from("scytale Code"), 6));
     println!("\"scytale Code\" size=8 -> {:?}", scytale_cipher(String::from("scytale Code"), 8));
-    println!("\"ABCDEFGH\" size=2 -> {:?}", scytale_cipher(String::from("ABCDEFGH"), 5));
+    println!("\"abcd\" size=2 -> {:?}", scytale_cipher(String::from("abcd"), 2));
 }
