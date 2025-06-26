926ccfef033cc596cb3449ccfb69dca10902bca2
kill <mustaphaboutoubdev@gmail.com>
2025-06-26 13:07:18 +0100

    same ref

--- how_many_references/src/lib.rs
+++ how_many_references/src/lib.rs
@@ -12,8 +12,7 @@ impl Node {
         self.ref_list.push(element)
     }
     pub fn rm_all_ref(&mut self, element: Rc<String>) {
-        self.ref_list
-            .retain(|a| *a != element);
+        self.ref_list.retain(|a| a.as_ptr() != element.as_ptr());
     }
 }
 
