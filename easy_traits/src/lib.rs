5d25919767cbb0bf7112c04a479708a4a8b28bbb
kill <mustaphaboutoubdev@gmail.com>
2025-06-28 16:37:35 +0100

    easy_traits

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -4,23 +4,25 @@ pub struct StringValue {
 }
 
 pub trait AppendStr {
-    fn append_str(&mut self, str_to_append: String);
+    fn append_str(&mut self, str_to_append: String) -> Self;
 
-    fn append_number(&mut self, nb_to_append: f64);
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
 
-    fn remove_punctuation_marks(&mut self);
+    fn remove_punctuation_marks(&mut self) -> Self;
 }
 
 impl AppendStr for StringValue {
-    fn append_str(&mut self, str_to_append: String) {
+    fn append_str(&mut self, str_to_append: String) -> Self {
         self.value += &str_to_append;
+        self.clone()
     }
 
-    fn append_number(&mut self, nb_to_append: f64) {
+    fn append_number(&mut self, nb_to_append: f64) -> Self {
         self.value += &nb_to_append.to_string();
+        self.clone()
     }
 
-    fn remove_punctuation_marks(&mut self) {
+    fn remove_punctuation_marks(&mut self) -> Self {
         let mut str = String::new();
         for ch in self.value.chars() {
             match ch {
@@ -29,5 +31,6 @@ impl AppendStr for StringValue {
             }
         }
         self.value = str;
+        self.clone()
     }
 }
