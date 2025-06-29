58ff976fc8f5af49e6ad0df7a80bbf6e5cc4bb83
kill <mustaphaboutoubdev@gmail.com>
2025-06-29 11:12:27 +0100

    algebra

---
+++ lalgebra_scalar/src/lib.rs
@@ -0,0 +1,65 @@
+pub trait Scalar: Sized {
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+    fn one() -> Self::Item {
+        1
+    }
+    fn zero() -> Self::Item {
+        0
+    }
+}
+
+impl Scalar for u32 {
+    type Item = i32;
+    fn one() -> Self::Item {
+        1
+    }
+    fn zero() -> Self::Item {
+        0
+    }
+}
+
+impl Scalar for u64 {
+    type Item = i32;
+    fn one() -> Self::Item {
+        1
+    }
+    fn zero() -> Self::Item {
+        0
+    }
+}
+
+impl Scalar for i64 {
+    type Item = i64;
+    fn one() -> Self::Item {
+        1
+    }
+    fn zero() -> Self::Item {
+        0
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f32;
+    fn one() -> Self::Item {
+        1.0
+    }
+    fn zero() -> Self::Item {
+        0.0
+    }
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+    fn one() -> Self::Item {
+        1.0
+    }
+    fn zero() -> Self::Item {
+        0.0
+    }
+}
