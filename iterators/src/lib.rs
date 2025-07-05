da6b39d414ba3cb8ccfd5852cfdd4dbbd574dd21
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-05 18:57:20 +0100

    closuers

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,34 +1,34 @@
+use std::iter::Enumerate;
+
 #[derive(Copy, Clone)]
 pub struct Collatz {
     pub v: u64,
 }
 
 impl Iterator for Collatz {
-    type Item = u64;
+    type Item = Self;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 1 || self.v == 0 {
+        let cc: Collatz = *self;
+        if self.v == 0 || self.v == 1 {
             None
-        } else if self.v % 2 == 0 {
-            self.v /= 2;
-            Some(self.v)
         } else {
-            self.v = (self.v * 3) + 1;
-            Some(self.v)
+            if self.v % 2 == 0 {
+                self.v /= 2;
+            } else {
+                self.v = self.v * 3 + 1;
+            }
+            Some(cc)
         }
     }
 }
 
 impl Collatz {
     pub fn new(n: u64) -> Self {
-        Self { v: n }
+        Self { v: n}
     }
 }
 
 pub fn collatz(n: u64) -> usize {
-    let mut collat = Collatz::new(n);
-    let mut step = 0 ;
-    while let Some(nbr) = collat.next() {
-        step += 1;
-    }
-    step
+    let collat = Collatz::new(n);
+    collat.count()
 }
