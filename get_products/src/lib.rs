30281b7f3c1e4ffc546698f3da43c2b5f88c88f0
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-05 16:54:31 +0100

    closuers

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
     let mut vec: Vec<usize> = vec![];
     for &ele in &arr {
-        vec.push(
-            arr.clone()
-                .into_iter()
-                .filter(|&a| a != ele)
-                .clone()
-                .reduce(|acc, e| acc * e)
-                .unwrap_or(0),
-        );
+        if let Some(e) = arr
+            .clone()
+            .into_iter()
+            .filter(|&a| a != ele)
+            .clone()
+            .reduce(|acc, e| acc * e)
+        {
+            vec.push(e);
+        }
     }
     vec
 }
--- get_products/src/main.rs
+++ get_products/src/main.rs
@@ -1,7 +1,35 @@
-use get_products::get_products;
 
+use get_products::*;
 fn main() {
     let arr: Vec<usize> = vec![1, 7, 3, 4];
     let output = get_products(arr);
     println!("{:?}", output);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_multiple() {
+        let arr: Vec<usize> = vec![1, 7, 3, 4];
+        let output = get_products(arr);
+        let arr2: Vec<usize> = vec![10, 3, 5, 6, 2];
+        let output2 = get_products(arr2);
+        assert_eq!(output, vec![84, 12, 28, 21]);
+        assert_eq!(output2, vec![180, 600, 360, 300, 900]);
+    }
+
+    #[test]
+    fn test_empty_case() {
+        let arr: Vec<usize> = Vec::new();
+        let output = get_products(arr);
+        assert_eq!(output, vec![]);
+    }
+
+    #[test]
+    fn test_single_case() {
+        let arr: Vec<usize> = vec![2];
+        let output = get_products(arr);
+        assert_eq!(output, vec![]);
+    }
 }
