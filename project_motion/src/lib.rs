c62d4e03005c485bb9018ee1432caa25c6873fc3
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-07-06 10:57:10 +0100

    closuers

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -29,19 +29,16 @@ impl Iterator for ThrowObject {
     type Item = Self;
     fn next(&mut self) -> Option<Self::Item> {
         self.time += 1.;
-        let a = 9.8;
-        let p_t_x = self.init_position.x
-            + (self.init_velocity.x * self.time)
-            + 0.5 * 0. * self.time * self.time;
+        let a = -9.8;
+        let mut p_t_x = self.init_position.x + (self.init_velocity.x * self.time);
         let p_t_y = self.init_position.y
-            - (self.init_velocity.y * self.time)
-            - 0.5 * a * self.time * self.time;
+            + (self.init_velocity.y * self.time)
+            + 0.5 * a * self.time * self.time;
         self.actual_position.x = round_1(p_t_x);
         self.actual_position.y = round_1(p_t_y);
 
-
-        let v_t_x = self.init_velocity.x + 0. * self.time;
-        let v_t_y = self.init_velocity.y - a * self.time;
+        let v_t_x = self.init_velocity.x;
+        let v_t_y = self.init_velocity.y + a * self.time;
         self.actual_velocity.x = round_1(v_t_x);
         self.actual_velocity.y = round_1(v_t_y);
         if self.actual_position.y <= 0. {
@@ -52,7 +49,6 @@ impl Iterator for ThrowObject {
     }
 }
 
-
 fn round_1(num: f32) -> f32 {
-    format!("{:.1}",num).parse().unwrap()
+    format!("{:.1}", num).parse().unwrap()
 }
